1453ba5f35d59a663a5aa65f95a097e742fad422
paizv <paizv@rose-hulman.edu>
2019-02-06 09:49:06 -0500

    fixed arm and claw - calibrate

--- src/rosebot.py
+++ src/rosebot.py
@@ -208,8 +208,12 @@ class ArmAndClaw(object):
           3. Resets the motor's position to 0.
         """
         self.raise_arm()
-        self.lower_arm()
         self.motor.reset_position()
+        self.motor.turn_on(-100)
+        while True:
+            if abs(self.motor.get_position()) >= 14.2 * 360:
+                self.motor.turn_off()
+                break
 
     def move_arm_to_position(self, desired_arm_position):
         """
