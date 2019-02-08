c6f3cfef1675928f312e607714b6078418228662
heinzjp <heinzjp@rose-hulman.edu>
2019-02-07 20:58:55 -0500

    Updated test for feature 1(arm and claw) Updated test for feature 4(drive system) (For demonstration)

--- src/m3_run_this_on_robot.py
+++ src/m3_run_this_on_robot.py
@@ -17,29 +17,60 @@ def main():
       1. Makes the EV3 robot to various things.
       2. Communicates via MQTT with the GUI code that runs on the LAPTOP.
     """
-    #run_test_arm()
-    #real_thing()
+    # run_test_arm()
     run_test_move()
+    run_test_beep()
+
+    # real_thing()
 
 
 def run_test_arm():
     robot = rosebot.RoseBot()
+    print('Running Test: Calibrate Arm')
+    robot.arm_and_claw.calibrate_arm()
+    print('Running Test: Move arm to Position')
+    robot.arm_and_claw.move_arm_to_position(2500)
+    time.sleep(2.5)
+    print('Running Test: Raise arm')
+    robot.arm_and_claw.raise_arm()
+    print('Running Test: Lower arm')
     robot.arm_and_claw.lower_arm()
 
 
 def real_thing():
     robot = rosebot.RoseBot()
-    delegate = shared_gui_delegate_on_robot.ResponderToGuiMessages(robot)
+    delegate = shared_gui_delegate_on_robot.ResponderToGUIMessage(robot)
     mqtt_receiver = com.MqttClient(delegate)
     mqtt_receiver.connect_to_pc()
     while True:
         time.sleep(0.01)
 
+
 def run_test_move():
     robot = rosebot.RoseBot()
-    robot.drive_system.go_straight_for_seconds(5, 100)
+    print('Running Test: Go')
+    robot.drive_system.go()
     time.sleep(5)
+    print('Running Test: Stop')
+    robot.drive_system.stop()
+    time.sleep(2.5)
+    print('Running Test: Go Straight for Seconds')
+    robot.drive_system.go_straight_for_seconds(5, 100)
+    time.sleep(2.5)
+    print('Running Test: Go Straight for Inches Using Time')
     robot.drive_system.go_straight_for_inches_using_time(10, 100)
+    time.sleep(2.5)
+    print('Running Test: Go Straight for Inches Using Encoder')
+    robot.drive_system.go_straight_for_inches_using_encoder(10, 100)
+    # More to be added for features 6 & 8
+
+
+def run_test_beep():
+    # will be added in feature 7
+    beeper = rosebot.Beeper()
+    beeper.beep().wait()
+
+
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
 # -----------------------------------------------------------------------------
