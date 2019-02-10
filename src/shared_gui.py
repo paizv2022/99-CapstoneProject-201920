f0a0e7cb554ebb38cf21016266b38c9a1ee0a284
paizv <paizv@rose-hulman.edu>
2019-02-10 18:09:33 -0500

    re added sound frame

--- src/shared_gui.py
+++ src/shared_gui.py
@@ -183,14 +183,44 @@ def get_driver_frame(window, mqtt_sender):
 
 
 def get_sound_frame(window, mqtt_sender):
-        pass
+    frame = ttk.Frame(window, padding=10, borderwidth=5, relief="ridge")
+    frame.grid()
 
-###############################################################################
-###############################################################################
-# The following specifies, for each Button,
-# what should happen when the Button is pressed.
-###############################################################################
-###############################################################################
+    frame_label = ttk.Label(frame, text="Sounds")
+    number_label = ttk.Label(frame, text="Number")
+    frequency_label = ttk.Label(frame, text="Frequency")
+    phrase_label = ttk.Label(frame, text='Phrase')
+    duration_label = ttk.Label(frame, text='Duration')
+
+    number_entry = ttk.Entry(frame, width=8)
+    frequency_entry = ttk.Entry(frame, width=8)
+    phrase_entry = ttk.Entry(frame, width=8)
+    duration_entry = ttk.Entry(frame, width=8)
+
+    beep_button = ttk.Button(frame, text='Beep')
+    tone_button = ttk.Button(frame, text='Tone')
+    phrase_button = ttk.Button(frame, text='Speak')
+
+    frame_label.grid(row=0, column=1)
+    number_label.grid(row=1, column=0)
+    frequency_label.grid(row=1, column=1)
+    duration_label.grid(row=1, column=2)
+    phrase_label.grid(row=1, column=3)
+
+    number_entry.grid(row=2, column=0)
+    frequency_entry.grid(row=2, column=1)
+    duration_entry.grid(row=2, column=2)
+    phrase_entry.grid(row=2, column=3)
+
+    beep_button.grid(row=3, column=0)
+    tone_button.grid(row=3, column=1)
+    phrase_button.grid(row=3, column=3)
+
+    beep_button["command"] = lambda: handle_beep(number_entry, mqtt_sender)
+    tone_button["command"] = lambda: handle_play_tone(frequency_entry, duration_entry, mqtt_sender)
+    phrase_button["command"] = lambda: handle_speak(phrase_entry, mqtt_sender)
+
+    return frame
 
 
 ###############################################################################
