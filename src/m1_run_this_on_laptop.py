2161495387c7c6853a2a03bd0e69a4fdce9d6811
paizv <paizv@rose-hulman.edu>
2019-02-10 17:53:23 -0500

    fixed m1

--- src/m1_run_this_on_laptop.py
+++ src/m1_run_this_on_laptop.py
@@ -40,7 +40,7 @@ def main():
     # -------------------------------------------------------------------------
     # Sub-frames for the shared GUI that the team developed:
     # -------------------------------------------------------------------------
-    teleop_frame, arm_frame, control_frame, sound_frame = get_shared_frames(main_frame, mqtt_sender)
+    teleop_frame, arm_frame, control_frame, driver_frame, sound_frame = get_shared_frames(main_frame, mqtt_sender)
 
     # -------------------------------------------------------------------------
     # Frames that are particular to my individual contributions to the project.
@@ -50,7 +50,7 @@ def main():
     # -------------------------------------------------------------------------
     # Grid the frames.
     # -------------------------------------------------------------------------
-    grid_frames(teleop_frame, arm_frame, control_frame, sound_frame)
+    grid_frames(teleop_frame, arm_frame, control_frame, driver_frame, sound_frame)
 
     # -------------------------------------------------------------------------
     # The event loop:
@@ -62,15 +62,17 @@ def get_shared_frames(main_frame, mqtt_sender):
     teleop_frame = shared_gui.get_teleoperation_frame(main_frame, mqtt_sender)
     arm_frame = shared_gui.get_arm_frame(main_frame, mqtt_sender)
     control_frame = shared_gui.get_control_frame(main_frame, mqtt_sender)
+    driver_frame = shared_gui.get_driver_frame(main_frame, mqtt_sender)
     sound_frame = shared_gui.get_sound_frame(main_frame, mqtt_sender)
 
-    return teleop_frame, arm_frame, control_frame, sound_frame
+    return teleop_frame, arm_frame, control_frame, driver_frame, sound_frame
 
 
-def grid_frames(teleop_frame, arm_frame, control_frame, sound_frame):
+def grid_frames(teleop_frame, arm_frame, control_frame, driver_frame, sound_frame):
     teleop_frame.grid(row=0, column=0)
     arm_frame.grid(row=1, column=0)
     control_frame.grid(row=2, column=0)
+    driver_frame.grid(row=0, column=1)
     sound_frame.grid(row=1, column=1)
 
 
